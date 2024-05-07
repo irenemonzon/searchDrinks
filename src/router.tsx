97fde90b5146f99a5e2394c57cfff7200409fba4
irenemonzon <irene.monzonm@gmail.com>
2024-05-07 14:42:07 +1000

    config tailwind, react router

---
+++ src/router.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import IndexPage from "./views/IndexPage"
+import FavoritePage from "./views/FavoritePage"
+import Layout from "./layouts/Layout"
+
+const Approuter = () => {
+  return (
+   <BrowserRouter>
+    <Routes>
+        <Route element={<Layout/>}>
+            <Route path='/' element={<IndexPage/>}index />
+            <Route path='/favoritos' element={<FavoritePage/>}/>  
+        </Route>
+    </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default Approuter
