971aec8173390de48fb2f0baaea69fed59e1ffff
irenemonzon <irene.monzonm@gmail.com>
2024-05-13 21:43:55 +1000

    improve performace with lazyload

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,21 @@
+import { lazy,Suspense } from "react"
 import { BrowserRouter, Routes,Route } from "react-router-dom"
 import IndexPage from "./views/IndexPage"
-import FavoritePage from "./views/FavoritePage"
 import Layout from "./layouts/Layout"
+const FavoritePage=lazy(()=>import('./views/FavoritePage'))
 
 const Approuter = () => {
+
   return (
    <BrowserRouter>
     <Routes>
         <Route element={<Layout/>}>
             <Route path='/' element={<IndexPage/>}index />
-            <Route path='/favoritos' element={<FavoritePage/>}/>  
+            <Route path='/favoritos' element={
+              <Suspense fallback="Cargando...">
+                <FavoritePage/>
+              </Suspense>
+            }/>  
         </Route>
     </Routes>
    </BrowserRouter>
